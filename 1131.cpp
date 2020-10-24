293797d6957d9adafc9a62c991edfbdee5e1d6b5
chenmuyuan <61893450+chenmuyuan@users.noreply.github.com>
2020-10-24 11:25:09 +0800

    Add files via upload

---
+++ 1131.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	if(n==1)
+		cout<<"192 384 576";
+	if(n==2)
+		cout<<"219 438 657";
+	if(n==3)
+		cout<<"273 546 819";
+	if(n==4)
+		cout<<"327 654 981";	
+} 
